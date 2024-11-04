625806cb613ee7edf6ca71c221280d57fe718e73
Akhonaa <aksmbatha@gmail.com>
2024-11-04 12:12:36 +0200

    init commit

---
+++ app/src/main/java/com/example/coffeeappv3/Activity/TrackerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.coffeeappv3.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.coffeeappv3.R
+
+class TrackerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tracker)
+    }
+}
